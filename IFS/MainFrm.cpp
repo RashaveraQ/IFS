8c88a2a54271b3988aab817c22328db14ced6bd5
akashi <akashi@18c69580-d92f-d848-b4a5-6284aa11c943>
2015-01-25 12:28:13 +0000

    ウィンドウのサイズ変更が出来ない問題を対処。
    git-svn-id: svn://localhost/src/IFS@631 18c69580-d92f-d848-b4a5-6284aa11c943

--- IFS/MainFrm.cpp
+++ IFS/MainFrm.cpp
@@ -36,10 +36,6 @@ BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
 		return FALSE;
 	// TODO: Modify the Window class or styles here by modifying
 	//  the CREATESTRUCT cs
-
-	cs.style = WS_OVERLAPPED | WS_CAPTION | FWS_ADDTOTITLE
-		 | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_MAXIMIZE | WS_SYSMENU;
-
 	return TRUE;
 }
 
